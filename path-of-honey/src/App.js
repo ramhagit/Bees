1c3bd06a2e38f504e32ef13bfdafd492c9975805
Hagit Ram <ram1hagit@gmail.com>
2021-07-13 17:42:40 +0300

    App.js cleared

--- path-of-honey/src/App.js
+++ path-of-honey/src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Bees
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://honeypath.org/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Bees
+          real site
         </a>
       </header>
     </div>
