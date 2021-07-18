3fec1b9633b45c81c85233629b4abf312e05bf14
Hagit Ram <ram1hagit@gmail.com>
2021-07-18 13:10:52 +0300

    Navbar style added

--- path-of-honey/src/components/Navbar/Navbar.js
+++ path-of-honey/src/components/Navbar/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import useWidth from '../../hooks/useWidth';
+import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <header>Navbar</header>
+        <header className="navbar-header">Navbar</header>
     )
 }
 
