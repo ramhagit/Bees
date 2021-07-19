3d7379d50660e33fe97501ffcd6875eeb9fc8289
Hagit Ram <ram1hagit@gmail.com>
2021-07-19 21:05:05 +0300

    links added to navbar

--- path-of-honey/src/components/Navbar/Navbar.js
+++ path-of-honey/src/components/Navbar/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import useWidth from '../../hooks/useWidth';
 import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <header className="navbar-header">Navbar</header>
+        <header className="navbar-header">Navbar
+        <div className="links">
+            <Link className="link" to="/" >Blog</Link>
+        </div>
+        </header>
     )
 }
 
