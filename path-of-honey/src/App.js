fcb1642aa23f13f435c2a82fd0fdd748dc4a5c37
Hagit Ram <ram1hagit@gmail.com>
2021-07-21 12:12:46 +0300

    route to blog added to App

--- path-of-honey/src/App.js
+++ path-of-honey/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Pages/Home/Home';
+import Blog from './components/Pages/Blog/Blog';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Navbar />
         <div className="pages">
           <Route path="/" exact component={Home} />
+          <Route path="/blog/" exact component={Blog} />
         </div>
       </BrowserRouter>
     </div>
